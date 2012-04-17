fa60da323c1cc7cf705457da3af33d1b17f0c994
Graeme Simpson <graeme@simmo.gs>
2012-04-17 14:15:59 +0200

    Add hook in for Wisepdf.

--- lib/wkhtmltopdf-heroku.rb
+++ lib/wkhtmltopdf-heroku.rb
@@ -15,3 +15,12 @@ begin
   WickedPdf.config[:exe_path] = WKHTMLTOPDF_PATH
 rescue LoadError
 end
+
+begin
+  require 'wisepdf'
+
+  Wisepdf::Configuration.configure do |config|       
+    config.wkhtmltopdf = WKHTMLTOPDF_PATH
+  end 
+rescue LoadError
+end
